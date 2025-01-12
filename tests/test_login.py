d7c50dd529c194cb349c5db1cd46c7ae62a6e94b
leasorssini <dlegasr@gmail.com>
2025-01-12 02:28:25 -0300

    funcional

--- tests/test_login.py
+++ tests/test_login.py
@@ -4,6 +4,9 @@ from unittest import TestCase
 from unittest.mock import patch
 from botocore.exceptions import ClientError
 
+# Defina as variáveis de ambiente **antes** da importação
+os.environ['COGNITO_USER_POOL_ID'] = 'your_user_pool_id'
+os.environ['COGNITO_CLIENT_ID'] = 'your_client_id'
 
 from src.login.login import lambda_handler, cognito_client
 
@@ -11,7 +14,6 @@ class TestLogin(TestCase):
 
     def setUp(self):
         # Configurar as variáveis de ambiente para cada teste individualmente
-        os.environ['COGNITO_USER_POOL_ID'] = 'us-east-1_ZL87UW5Jl'
         os.environ['COGNITO_USER_POOL_ID'] = 'your_user_pool_id'
         os.environ['COGNITO_CLIENT_ID'] = 'your_client_id'
 
